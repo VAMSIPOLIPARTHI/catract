4155f36ae2177260f5474cc06ec5ca792530efee
Poliparthi Vamsi <152620111+VAMSIPOLIPARTHI@users.noreply.github.com>
2026-01-13 04:30:09 +0530

    Update API_BASE URL for production deployment

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -1,7 +1,8 @@
 // config.js - Global Configuration
 
 // Change this URL when deploying to production
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://vamsi1103-cataract-backends.hf.space/";
 
 // Other global settings can be added here
 const APP_NAME = "CataractDetect AI";
+
