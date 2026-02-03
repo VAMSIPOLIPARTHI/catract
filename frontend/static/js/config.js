ed5ef832ac49634b5262b446e1cc80adb9757238
Poliparthi Vamsi <152620111+VAMSIPOLIPARTHI@users.noreply.github.com>
2026-02-03 23:19:38 +0530

    Update config.js

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -1,8 +1,10 @@
 // config.js - Global Configuration
 
 // Change this URL when deploying to production
-const API_BASE = "https://vamsi1103-cataract-backends.hf.space/";
+const API_BASE = "https://vamsi1103-cataract-backend.hf.space/";
 
 // Other global settings can be added here
 const APP_NAME = "CataractDetect AI";
 
+
+https://huggingface.co/spaces/vamsi1103/cataract-backend
